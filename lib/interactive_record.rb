e6488552e894bbc0682d2b2c63ea215985bf7a0f
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-28 03:14:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,7 @@ class InteractiveRecord
   end
 
   def self.find_by(param={})
-    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+    sql = "SELECT * FROM #{self.table_name} WHERE '?' = ?"
     DB[:conn].execute(sql, param.keys.join, param.values.join)
   end
 
