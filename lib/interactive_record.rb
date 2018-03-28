5709782c3232d42d692e8e3dbff7b70ad078b5fa
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-28 02:05:24 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,7 +29,7 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.join(", ")
+    self.class.column_names.delete_if{|name| name = "id"}.join(", ")
   end
 
 end
