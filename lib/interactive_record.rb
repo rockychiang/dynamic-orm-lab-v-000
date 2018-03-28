1ac01eb7b8fff5dd171c84d7e719b77a03dd3955
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-28 01:56:30 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,23 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    col_names = []
+    
+    table_info.each do |column|
+      col_names << column["name"]
+    end
+    col_names.compact
+  end
+
+  def initialize(attributes={})
+
+  end
+  
 end
