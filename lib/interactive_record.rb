f665beb4bbf0d13f83ecd49b85ad3b6d1fbfd0da
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-28 02:21:26 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,4 +49,8 @@ class InteractiveRecord
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name_for_insert} WHERE name = ?"
+  end
+
 end
